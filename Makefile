3333c81758c39530b5b66a77252895fcd55c49c5
manuel <manuel.l.soares@tecnico.ulisboa.pt>
2023-05-12 18:08:01 +0100

    added makefile

---
+++ Makefile
@@ -0,0 +1,5 @@
+
+
+setup:
+	cp src/conf/extensions.conf /etc/asterisk
+	cp src/conf/sip.conf /etc/asterisk
--- src/scripts/stock.py
+++ src/scripts/stock.py
@@ -1,19 +1,29 @@
 from requests import get, Response
+from urllib3.connection import NewConnectionError
 from html.parser import HTMLParser
 from bs4 import BeautifulSoup
 
+
 def get_stock_price():
-    url = "http://www.qontigo.com/index/sxxp/"
-    response : Response = get(url)
-    match response.status_code:
-        case 200:
-            get_stock_price_from_html(response.content)
-        case _:
-            print(f"Request failed with error code: {response.status_code}")
+    url = "https://www.qontigo.com/index/sxxp/"
+
+    response: Response = get(url)
+    match response.status_code, get_stock_price_from_html(response.content):
+        case 200, price:
+            print(f"The price of the SXXP index is {price} Euro")
+        case 200, None:
+            print(f"We are sorry but your request failed with error code 1")
+        case status, _:
+            print(
+                f"We are sorry but your request failed with error code {status}")
 
 
 def get_stock_price_from_html(html):
-    soup = BeautifulSoup(html)
+    soup = BeautifulSoup(html, features='lxml')
     price_span = soup.find('span', id='overview-last-value')
-    print(price_span.contents)
+    if price_span:
+        return price_span.contents[0]
+    return None
+
+
 get_stock_price()
